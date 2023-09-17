b07b66b2c79b0bb6303642fff91a388c649f688a
IMS <inisoetan2014@gmail.com>
2023-09-17 21:01:30 +0100

    feat: remove bootstrap and use flex

--- public/index.js
+++ public/index.js
@@ -1,42 +1,47 @@
-function hideSideNav(){
-    $('.side-nav').hide()
-    $('.overlay').hide()
-    $('#harmbuger').show()
+function hideSideNav() {
+  $(".side-nav").hide();
+  $(".overlay").hide();
+  $("#harmbuger").show();
 }
-function showSideNav(){
-    $('.overlay').show()
-    $('.side-nav').show()
-    $('#harmbuger').hide()
-}
-$(document).ready(
-    ()=>{
-    $('.overlay').hide()
-})
-$('#harmbuger').click(()=>{
-   showSideNav()
-})
-$('#close').click(()=>{
-    hideSideNav()
-})
-$('.side-nav-link').click(()=>{
-    hideSideNav()
-})
 
-const scroll = 700
-function scrollContainer(){
-    return document.documentElement || document.body
+function showSideNav() {
+  $(".overlay").show();
+  $(".side-nav").show();
+  $("#harmbuger").hide();
 }
-function goToTop (){
-    return document.body.scrollIntoView()
+
+$(document).ready(() => {
+  $(".overlay").hide();
+});
+
+$("#harmbuger").click(() => {
+  showSideNav();
+});
+
+$("#close").click(() => {
+  hideSideNav();
+});
+
+$(".side-nav-link").click(() => {
+  hideSideNav();
+});
+
+const scroll = 700;
+
+function scrollContainer() {
+  return document.documentElement || document.body;
 }
-$(document).scroll(()=>{
-    if (scrollContainer().scrollTop > scroll){
-        $('.back-to-top-btn').css('visibility','visible')
-    }
-    else{
-        $('.back-to-top-btn').css('visibility','hidden')
-    }
-})
-$('.back-to-top-btn').click(()=>
-    goToTop()
-)
+
+function goToTop() {
+  return document.body.scrollIntoView();
+}
+
+$(document).scroll(() => {
+  if (scrollContainer().scrollTop > scroll) {
+    $(".back-to-top-btn").css("visibility", "visible");
+  } else {
+    $(".back-to-top-btn").css("visibility", "hidden");
+  }
+});
+
+$(".back-to-top-btn").click(() => goToTop());
